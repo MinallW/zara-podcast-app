94d4440f5dae5e22faad5297288ef66fdf124aae
Miguel <suarezmiguelc@icloud.com>
2023-08-17 00:56:02 +0200

    Pagination <- BEAUTIFUL

--- app/page.tsx
+++ app/page.tsx
@@ -3,10 +3,14 @@
 import { useState, useEffect } from 'react'
 import Podcast from './components/Podcast'
 import Input from '@mui/joy/Input';
+import Pagination from '@mui/material/Pagination';
+import Stack from '@mui/material/Stack';
 
 export default function Home() {
   const [data, setData] = useState([])
   const [isLoading, setLoading] = useState(true)
+  const [currentPage, setCurrentPage] = useState(1);
+  const itemsPerPage = 20;
 
   useEffect(() => {
     fetch('https://itunes.apple.com/us/rss/toppodcasts/limit=100/genre=1310/json')
@@ -20,13 +24,19 @@ export default function Home() {
 
   if (isLoading) return <p>Loading...</p>
 
+  // Calculate the start and end index of items for the current page
+  const startIndex = (currentPage - 1) * itemsPerPage;
+  const endIndex = startIndex + itemsPerPage;
+  // Extract the items to display for the current page
+  const itemsToDisplay = data.slice(startIndex, endIndex)
+
   return (
     <>
       <div className='flex items-center justify-around'>
         <Input size="lg" className='m-8 z-0' />
       </div>
       <main className="flex min-h-screen flex-wrap items-center justify-around p-24 gap-24">
-        {data.map((podcast, index) => {
+        {itemsToDisplay.map((podcast, index) => {
           return (
             <Podcast
               key={index}
@@ -37,7 +47,13 @@ export default function Home() {
             />
           )
         })}
-      </main>
+        <Pagination
+          count={Math.ceil(data.length / itemsPerPage)} // Calculate total number of pages
+          page={currentPage}
+          variant="outlined"
+          shape="rounded"
+          onChange={(event, page) => setCurrentPage(page)} // Handle page change
+        />      </main>
     </>
 
   )
