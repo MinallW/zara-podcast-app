541a23278010765f8d3be2867ed7faa481ddd5fb
Miguel <suarezmiguelc@icloud.com>
2023-08-16 22:06:24 +0200

    updated way to do breadcrumbs, also MUI!

--- app/components/NavBarBreadcrumbs.jsx
+++ app/components/NavBarBreadcrumbs.jsx
@@ -1,37 +1,24 @@
+'use client'
+
+import KeyboardArrowRight from '@mui/icons-material/KeyboardArrowRight';
+import Breadcrumbs from '@mui/joy/Breadcrumbs';
+import Link from '@mui/joy/Link';
+import Typography from '@mui/joy/Typography';
+import { usePathname } from 'next/navigation'
+
 export default function NavBarBreadcrumbs() {
+    const pathname = usePathname()
+    console.log(pathname)
+
     return (
-        <div className="flex" aria-label="Breadcrumb">
-            <ol className="flex items-center space-x-1 md:space-x-3">
-                <li className="inline-flex items-center">
-                    <a href="#" className="inline-flex items-center text-sm font-medium text-gray-700 hover:text-blue-600 dark:text-gray-400 dark:hover:text-white">
-                        Inicio
-                    </a>
-                </li>
-                <BreadcrumbLink LinkTitle={'yes'}/>
-                <li>
-                    <div className="flex items-center">
-                        <svg className="w-3 h-3 text-gray-400 mx-1" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 6 10">
-                            <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m1 9 4-4-4-4" />
-                        </svg>
-                        <span className="ml-1 text-sm font-medium text-gray-500 md:ml-2 dark:text-gray-400">Episodio</span>
-                    </div>
-                </li>
-            </ol>
-        </div>
-    )
+        <Breadcrumbs separator={<KeyboardArrowRight />} aria-label="breadcrumbs">
+            <Typography>Amy</Typography>
+            {['Characters', 'Futurama', 'TV Shows', 'Home'].map((item) => (
+                <Link key={item} color="neutral" href="#separators">
+                    {item}
+                </Link>
+            ))}
+        </Breadcrumbs>
+    );
 }
 
-function BreadcrumbLink({ LinkTitle }) {
-    return (
-        <>
-            <li>
-                <div className="flex items-center">
-                    <svg className="w-3 h-3 text-gray-400 mx-1" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 6 10">
-                        <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m1 9 4-4-4-4" />
-                    </svg>
-                    <a href="#" className="ml-1 text-sm font-medium text-gray-700 hover:text-blue-600 md:ml-2 dark:text-gray-400 dark:hover:text-white">{LinkTitle}</a>
-                </div>
-            </li>
-        </>
-    )
-}
