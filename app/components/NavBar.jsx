77a1679670e2be88be0596775f3027fff8816bb0
Miguel <suarezmiguelc@icloud.com>
2023-08-17 10:32:59 +0200

    Navbar z-index fix

--- app/components/NavBar.jsx
+++ app/components/NavBar.jsx
@@ -6,7 +6,7 @@ import NavBarSpinner from './NavBarSpinner'
 export default function Navbar() {
     return (
         <>
-            <div className="w-full h-20 bg-gray-300 sticky top-0">
+            <div className="w-full h-20 bg-gray-300 sticky top-0 z-10">
                 <div className="container mx-auto px-4 h-full">
                     <div className="flex justify-between items-center h-full">
                         <Logo />
